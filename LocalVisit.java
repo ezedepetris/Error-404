416bf3dc3c2d68419c97f598475c6b9f7bc025d4
ezequiel <ezedepetris@gmail.com>
2015-04-18 16:58:58 -0300

    class partido con antributos local y visitantes

--- LocalVisit.java
+++ LocalVisit.java
@@ -1,12 +1,15 @@
 public class LocalVisit{
-/*clase que representa un partido en la parte de local va el equipo local y en el atributo visitante el equipo visitante*/
-	private String local = "";
-	private String visit = "";
 
-	public LocalVisit(String lo, String vi){
-		local = lo;
-		visit = vi;
+/*clase que representa un partido en la parte de local va el equipo local y en el atributo visitante el equipo visitante*/
+	private String local ;
+	private String visit ;
+
+// constructor
+	public LocalVisit(){
+		local = "";
+		visit = "";
 	}
+
 /*set's and get's*/
 	public void setLocal(String value){
 		local = value;
