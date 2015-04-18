cc3ee1640cb43b27110d33e52e0377e3aacec004
ezequiel <ezedepetris@gmail.com>
2015-04-18 19:15:29 -0300

    Test fixture

---
+++ Test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class Test {
+	public static void main(String[] args) {
+		ArrayList<String> arreglo = new ArrayList<String>();
+		arreglo.add("1");
+		arreglo.add("2");
+		arreglo.add("3");
+		arreglo.add("4");
+		arreglo.add("5");
+		arreglo.add("6");
+		arreglo.add("7");
+		arreglo.add("8");
+		arreglo.add("9");
+		arreglo.add("10");
+		Fixture.fixture(arreglo);
+	}
+}
