0b90e2e721b01183c7308381defa7e6c38962564
gamassimino <gamassimino01@gmail.com>
2015-06-08 11:47:37 -0300

    samuel

--- Atom/AtomState.java
+++ Atom/AtomState.java
@@ -1,32 +1,51 @@
 import libraries.abstract.State;
 import java.util.*;
 
-public class AtomState implements State{
-	Atom[][] board ;
+public class AtomsState implements AdversarySearchState{
+	Atoms[][] board ;
+	boolean flag;
 	
-	public AtomState(){
-		board = new Atom[6][10];
-		board[0][0] = new Atom(1);
-		board[5][9] = new Atom(2);
+	public AtomsState(){
+		board = new Atoms[6][10];
+		board [0][0] = new Atom(1);
+		board [5][9] = new Atom(2);
 	}
 
-	public AtomState(int x, int y,Atom element){
+	public AtomsState(int x, int y,Atoms element){
 		board[x][y] = element;
 	}
 	
-	public boolean equals(State s){
-
+	public boolean equals(AdversarySearchState s){
+		boolean flag = true;
+		for (int i=0; i<6; i++) {
+			for (int j = 0; j<10; j++) {
+				if (this[i][j]!=null){
+					if (this[i][j].getNumber()!=s[i][j].getNumber() || this[i][j].getPlayer()!=s[i][j].getPlayer())
+						flag = false;
+				}
+			}
+		}
+		return flag;
 	}
 
 	public String toString(){
-		String string ="";
-		for (int i = 0; i<6; i++) {
+		String string = "";
+		for (int i=0; i<6; i++) {
 			for (int j = 0; j<10; j++) {
-				string += board[x][y]+" "
+				string += board[x][y]+" ";
 			}
-			string +="\n";
+			string += "\n"
 		}
 		return string;
+	}
+
+	public boolean isMax(){
+		return flag;
+
+	}
+
+	public Object ruleApplied(){
+		return null;
 	}
 
 }
