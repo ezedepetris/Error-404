26c4a3556918c461b57e26f7400f2158fbe3d7b1
gamassimino <gamassimino01@gmail.com>
2015-06-08 10:50:44 -0300

    new project

---
+++ Atom/Atom.java
@@ -0,0 +1,23 @@
+public class Atom{
+
+	private int player;
+	private int number;
+
+	public Atom(int p){
+		player = p;
+		number = 1;
+	}
+
+	public int getNumber(){
+		return number;
+	}  
+
+	public int getPlayer(){
+		return player;
+	}
+
+	public void setAtom(int p){
+		player = p;
+		number++;
+	}
+}
