23c88ba583fc47ef505223977c06e879b3537d7b
ezequiel <ezedepetris@gmail.com>
2015-06-08 19:19:44 -0300

    get value ready

--- Atom/AtomState.java
+++ Atom/AtomState.java
@@ -1,26 +1,31 @@
-import libraries.abstract.State;
+import libraries.abstract.AdversarySearchState;
 import java.util.*;
 
-public class AtomsState implements AdversarySearchState{
+public class AtomState implements AdversarySearchState{
 	Atoms[][] board ;
 	boolean flag;
 	
-	public AtomsState(){
-		board = new Atoms[6][10];
+	public AtomState(){
+		board = new Atom[6][10];
 		board [0][0] = new Atom(1);
 		board [5][9] = new Atom(2);
 	}
 
-	public AtomsState(int x, int y,Atoms eslement){
-		board[x][y] = element;
+	public AtomState(int x, int y,Atom eslement){
+		if(board[x][y]==null)
+			board[x][y] = element;
+		else
+			board[x][y] = board[x][y].setAtom(element.getPlayer());
 	}
 	
 	public boolean equals(AdversarySearchState s){
 		boolean flag = true;
 		for (int i=0; i<6; i++) {
 			for (int j = 0; j<10; j++) {
-				if (this.[i][j]!=null){
-					if (this.[i][j].getNumber()!=s[i][j].getNumber() || this.[i][j].getPlayer()!=s[i][j].getPlayer())
+				if((this.board.[i][j]!=null && s.board.[i][j]==null) || (s.board.[i][j]!=null && this.board.[i][j]==null))
+					return false;
+				if (this.board.[i][j]!=null && s.board.[i][j]!=null){//falta retornar false si alguna de las dos es null
+					if (this.board.[i][j].getNumber()!=s.board.[i][j].getNumber() || this.board.[i][j].getPlayer()!=s.board.[i][j].getPlayer())
 						flag = false;
 				}
 			}
